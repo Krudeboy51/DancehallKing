b5d4305f4347d04a5e721b582309352cf48a0b9a
Kory King <kking9351@gmail.com>
2016-10-07 01:28:50 -0400

    first commit

---
+++ app/src/main/java/krudeboy/dancehallking/HomeFragment.java
@@ -0,0 +1,21 @@
+package krudeboy.dancehallking;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kory on 10/6/2016.
+ */
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.search_fragment_layout, container, false);
+
+        return view;
+    }
+}
